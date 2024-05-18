27c55e10478707abc37f48b0e4d77c7bc4c06daa
Arimurilloh <161553052+Arimurilloh@users.noreply.github.com>
2024-05-18 19:08:04 +0000

    Se crea la TodoList personalizada

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = () => {
+    if (inputValue) {
+      setTasks([...tasks, inputValue]);
+      setInputValue("");
+    }
+  };
+
+  const deleteTask = (index) => {
+    setTasks(tasks.filter((task, i) => i !== index));
+  };
+
+  return (
+    <div className="container">
+      <h1>TodoList</h1>
+      <ul>
+        <li>
+          <input
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                addTask();
+              }
+            }}
+            placeholder="Write your task here"
+          />
+        </li>
+        {tasks.map((item, index) => (
+          <li key={index} className="task-item">
+            <span>{item}</span>
+            <i
+              className="fa-solid fa-square-minus delete-icon"
+              onClick={() => deleteTask(index)}
+            ></i>
+          </li>
+        ))}
+      </ul>
+      <div>{tasks.length} tasks</div>
+    </div>
+  );
 };
 
 export default Home;
